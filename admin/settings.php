2a6cb8a2452cbbed08c458e764cce8f524bfac5c
tranbao <lamtrunghieu366@gmail.com>
2024-01-23 10:57:19 +0700

    update final for release
    update final for release

--- admin/settings.php
+++ admin/settings.php
@@ -7,11 +7,11 @@ add_action( 'carbon_fields_register_fields', 'mje_profiles_list_settings',999,0 
 function mje_profiles_list_settings()
 {
     
-    Container::make( 'theme_options', __( 'MJE Profiles list Settings', 'crb' ) )
+    Container::make( 'theme_options', __( 'MJE Profiles list Settings', 'crb' ) )     
     ->set_icon( 'dashicons-admin-generic')
-    ->set_page_menu_title( 'MJE Profile List Settings' )
-    ->set_page_menu_position(4)
-  ->add_tab( __( 'General' ), array(
+    ->set_page_menu_title( 'MJE Profile List Settings' )    
+    ->set_page_menu_position(6)    
+  ->add_tab('General', array(
    Field::make( 'text', 'mje_profile_number_of_profiles', __( 'Number of profiles to show' ) )->set_default_value(10)->set_width(30),
    Field::make( 'select', 'mje_profile_sort_by', __( 'Sort by' ) )->set_options(array('createdDate'=>'Created date','highrating'=>'High Rating','nummjob'=>'Number of Mjob'))->set_width(30),        
    Field::make( 'select', 'mje_profile_order', __( 'Order' ) )->set_options(array('asc'=>'Asc','desc'=>'Desc'))->set_width(30),        
@@ -32,22 +32,23 @@ function mje_profiles_list_settings()
     Field::make( 'select', 'mje_profile_carousel_navigation_style', __( 'Slider Navigation Style' ) )->set_options(array('classic'=>'Button Navigation','newstyle'=>'New Style','both'=>'Both'))->set_width(20),              
 
     Field::make( 'radio', 'mje_profile_carousel_freemode', __( 'Fast mode' ) )->set_options(array('true'=>'Yes','false'=>'No'))->set_width(20),              
-
-    Field::make( 'html', 'display_shortcode_mje_profile', __( 'Shortcode' ) )
-	->set_html('<p> <strong> Shortcode </strong> </p>[custom_profile_carousel_slider] <h4><a target="_blank" href="https://docs.enginethemes.com/article/602-how-to-use-mje-profile-list-plugin">Setup Document</a> </h4>'),
-
-
+    Field::make( 'html', 'display_shortcode_mje_profile', __( 'Shortcode' ) )->set_html('<p> <strong> Shortcode </strong> </p>[custom_profile_carousel_slider]')
+    ->set_html('<input type="submit" value="Save Changes" name="publish" id="publish" class="button button-primary button-large">'),
     )) 
-  ->add_tab( __( 'Replacing text' ), array(
-    Field::make( 'text', 'mje_profile_list_title', __( 'Title of list' ) )->set_default_value('Newest Profiles'),      
+  ->add_tab( 'Replacing text', array(
+    Field::make( 'text', 'mje_profile_list_title', __( 'Title of list' ) )->set_default_value('Latest Profiles'),      
     Field::make( 'text', 'mje_profile_null_profile_description', __( 'Empty profile description' ) )->set_default_value('This is the description of prorfile but the user did not enter the description'),    
     Field::make( 'text', 'mje_profile_service_text', __( 'Services' ) )->set_default_value('Services'),    
     Field::make( 'text', 'mje_profile_review_text', __( 'Reviews' ) )->set_default_value('Reviews'),    
     Field::make( 'text', 'mje_profile_null_country', __( 'None ( Text for empty country )' ) )->set_default_value('None'),    
     Field::make( 'text', 'mje_profile_null_language', __( 'None ( Text for empty language )' ) )->set_default_value('None'),    
-    Field::make( 'text', 'mje_profile_view_profile_button', __( 'View Profile' ) )->set_default_value('View Profile'),          
+    Field::make( 'text', 'mje_profile_view_profile_button', __( 'View Profile' ) )->set_default_value('View Profile'),    
+    Field::make( 'html', 'display_button_submit_test', __( 'Shortcode' ) )
+	->set_html('<input type="submit" value="Save Changes" name="publish" id="publish" class="button button-primary button-large">'),      
        )) 
-  ->add_tab( __( 'Style' ), array(    
+
+       
+  ->add_tab('Style', array(    
     Field::make( 'color', 'mje_profile_slide_wrappger_bg_color', 'Container Background Color' )->set_default_value('#ffffff'),  
     Field::make( 'color', 'mje_profile_slide_item_header_color', 'Slide Item Header color' )->set_default_value('#10A2F6')->set_width(50),  
     Field::make( 'color', 'mje_profile_rate_star_color', 'Rating Star Color' )->set_default_value('#10a2ef')->set_width(50),  
@@ -56,10 +57,32 @@ function mje_profiles_list_settings()
     Field::make( 'color', 'mje_profile_slide_navigation_bg_color', 'Navigation Background color' )->set_default_value('#10A2F6')->set_width(30),  
     Field::make( 'color', 'mje_profile_slide_navigation_text_color', 'Navigation Text color' )->set_default_value('#ffffff')->set_width(30),  
     Field::make( 'text', 'mje_profile_slide_navigation_opacity', 'Navigation Opacity' )->set_attribute('placeholder','Min: 0.1 - Max: 1')->set_default_value(0.5)->set_width(30),  
+    Field::make( 'html', 'display_button_submit', __( 'Shortcode' ) )
+	->set_html('<input type="submit" value="Save Changes" name="publish" id="publish" class="button button-primary button-large">'),
+    ))
 
-
-       ))       
+  ->add_tab('Support',array(
+    Field::make( 'html', 'display_document_guide', __( 'Shortcode' ) )
+    ->set_html('<h4>You can read the documentation <a target="_blank" href="https://docs.enginethemes.com/article/602-how-to-use-mje-profile-list-plugin"> here.</a> </h4> <h4>Drop us a support ticket <a href="https://www.enginethemes.com/help/i-have-a-basic-question/" target="_blank"> here!</a></h4>'),
+  ))
 
   ;
 }
-//add tab for change color option , change text...
+
+add_action('admin_head','hide_default_submit_button');
+
+function hide_default_submit_button()
+{
+  ?>
+  <style>
+    #post-body-content #carbon_fields_container_mje_profiles_list_settings
+    {
+      width:100% !important;
+    }
+    #postbox-container-1
+    {
+      display:none !important;
+    }
+  </style>
+  <?php
+}
--- mje-profiles-list.php
+++ mje-profiles-list.php
@@ -1,45 +1,35 @@
 <?php
 
 /**
- * The plugin bootstrap file
  *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://enginethemes.com
+ * @link              https://www.enginethemes.com
  * @since             1.0.0
  * @package           Mje_Profiles_List
  *
  * @wordpress-plugin
- * Plugin Name:       MJE Profiles List
+ * Plugin Name:       MjE Profile List
  * Plugin URI:        https://enginethemes.com
- * Description:       an awesome plugin to create the profiles list
+ * Description:       an MjE plugin to add the profile list block to homepage
  * Version:           1.0.0
  * Author:            EngineThemes
- * Author URI:        https://enginethemes.com/
+ * Author URI:        https://www.enginethemes.com/
  * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       mje-profiles-list
  * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'MJE_PROFILES_LIST_VERSION', '1.0.0' );
+
+define('MJE_PROFILES_LIST_VERSION', '1.0.0');
 
 //custom code
-define( 'MJE_PROFILES_LIST_PATH', dirname( __FILE__ ) );
-define( 'MJE_PROFILES_LIST_URL', plugin_dir_url( __FILE__ ) );
+define('MJE_PROFILES_LIST_PATH', dirname(__FILE__));
+define('MJE_PROFILES_LIST_URL', plugin_dir_url(__FILE__));
 
 //end
 
@@ -47,8 +37,9 @@ define( 'MJE_PROFILES_LIST_URL', plugin_dir_url( __FILE__ ) );
  * The code that runs during plugin activation.
  * This action is documented in includes/class-mje-profiles-list-activator.php
  */
-function activate_mje_profiles_list() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-mje-profiles-list-activator.php';
+function activate_mje_profiles_list()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-mje-profiles-list-activator.php';
 	Mje_Profiles_List_Activator::activate();
 }
 
@@ -56,19 +47,20 @@ function activate_mje_profiles_list() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-mje-profiles-list-deactivator.php
  */
-function deactivate_mje_profiles_list() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-mje-profiles-list-deactivator.php';
+function deactivate_mje_profiles_list()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-mje-profiles-list-deactivator.php';
 	Mje_Profiles_List_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_mje_profiles_list' );
-register_deactivation_hook( __FILE__, 'deactivate_mje_profiles_list' );
+register_activation_hook(__FILE__, 'activate_mje_profiles_list');
+register_deactivation_hook(__FILE__, 'deactivate_mje_profiles_list');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-mje-profiles-list.php';
+require plugin_dir_path(__FILE__) . 'includes/class-mje-profiles-list.php';
 
 /**
  * Begins execution of the plugin.
@@ -79,11 +71,11 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-mje-profiles-list.php';
  *
  * @since    1.0.0
  */
-function run_mje_profiles_list() {
+function run_mje_profiles_list()
+{
 
 	$plugin = new Mje_Profiles_List();
 	$plugin->run();
-
 }
 run_mje_profiles_list();
 
@@ -91,7 +83,7 @@ run_mje_profiles_list();
 function require_custom_subscription_mje_files()
 {
 	require_once MJE_PROFILES_LIST_PATH . '/includes/functions.php';
-	require_once MJE_PROFILES_LIST_PATH . '/admin/settings.php';	
+	require_once MJE_PROFILES_LIST_PATH . '/admin/settings.php';
 }
-add_action( 'after_setup_theme', 'require_custom_subscription_mje_files' ); 
+add_action('after_setup_theme', 'require_custom_subscription_mje_files'); 
 //end
